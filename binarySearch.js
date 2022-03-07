c78fce5cb6f0db8b8b74730a07ad213ac1bac9f0
Mike <mikeclosky29@gmail.com>
2022-03-07 09:23:35 -0600

    string search

--- binarySearch.js
+++ binarySearch.js
@@ -15,3 +15,20 @@ const binarySearch = (array, num) => {
     if(arr[middle] === num) return middle
     return -1
 }
+
+
+// Binary Search condensed
+const binarySearch = (array, num) => {
+    let start = 0
+    let end = array.length - 1
+    let middle = Math.floor((start + end)/2)
+    while(arr[middle] !== num && start <= end){
+        if(arr[middle] < num)start = middle + 1 
+            else end = middle - 1
+            middle = Math.floor((start + end)/2)
+    }
+    return arr[middle] ? middle : -1;
+}
+
+//Time and Space Complexity:
+//Time: O(log n) Space: O(1)
