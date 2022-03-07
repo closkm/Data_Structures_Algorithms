b03b818578065b9aaedef931c3f5974a3cd5b6c1
Mike <mikeclosky29@gmail.com>
2022-03-07 08:54:58 -0600

    Binary Search

---
+++ binarySearch.js
@@ -0,0 +1,17 @@
+
+// Binary Search, array must be sorted
+const binarySearch = (array, num) => {
+    let start = 0
+    let end = array.length - 1
+    let middle = Math.floor((start + end)/2)
+    while(arr[middle] !== num && start <= end){
+        if(arr[middle] < num){
+            start = middle + 1 //add or subtract one because it is redunant to search same middle value that is not the target number
+        }else{
+            end = middle - 1
+        }
+        middle = Math.floor((start + end)/2)
+    }
+    if(arr[middle] === num) return middle
+    return -1
+}
